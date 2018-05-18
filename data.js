0a1297ae8fc526f444a55748a5a7975ab05c3ba8
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2018-05-18 13:27:11 +0200

    Add personas and tasks data

---
+++ data.js
@@ -0,0 +1,140 @@
+module.exports.personas = {
+  'Ellen Who': {
+    quote: '',
+    description: '',
+    happiness: 6, // 0-6
+    energy: 10, // 0-10
+    scores: {
+      security: 2, // 0-8
+      nature: 6, // 0-8
+      culture: 4, // 0-8
+      money: 8, // 0-8
+      social: 0, // 0-8
+    }
+  },
+  'Greet de Jong': {
+    quote: '',
+    description: '',
+    happiness: 6, // 0-6
+    energy: 6, // 0-10
+    scores: {
+      security: 0, // 0-8
+      nature: 6, // 0-8
+      culture: 8, // 0-8
+      money: 4, // 0-8
+      social: 2, // 0-8
+    }
+  },
+  'Brad Tipp': {
+    quote: '',
+    description: '',
+    happiness: 6, // 0-6
+    energy: 8, // 0-10
+    scores: {
+      security: 2, // 0-8
+      nature: 6, // 0-8
+      culture: 4, // 0-8
+      money: 8, // 0-8
+      social: 0, // 0-8
+    }
+  },
+  'Ping Lee': {
+    quote: '',
+    description: '',
+    happiness: 6, // 0-6
+    energy: 6, // 0-10
+    scores: {
+      security: 0, // 0-8
+      nature: 2, // 0-8
+      culture: 8, // 0-8
+      money: 6, // 0-8
+      social: 4, // 0-8
+    }
+  },
+  'Naru Muhammadu': {
+    quote: '',
+    description: '',
+    happiness: 6, // 0-6
+    energy: 8, // 0-10
+    scores: {
+      security: 4, // 0-8
+      nature: 8, // 0-8
+      culture: 2, // 0-8
+      money: 6, // 0-8
+      social: 0, // 0-8
+    }
+  }
+}
+
+module.exports.tasks = {
+  'Markin\' the law': {
+    task: '',
+    taskDescription: '',
+    resultDescription: '',
+    scores: {
+      security: 30,
+      nature: 0,
+      culture: -10,
+      money: 0,
+      social: 10,
+    },
+    threshold: 10,
+    currentValue: 0
+  },
+  'I want to believe': {
+    task: '',
+    taskDescription: '',
+    resultDescription: '',
+    scores: {
+      security: -20,
+      nature: 0,
+      culture: 20,
+      money: -10,
+      social: 0,
+    },
+    threshold: 10,
+    currentValue: 0
+  },
+  'A perfect stranger': {
+    task: '',
+    taskDescription: '',
+    resultDescription: '',
+    scores: {
+      security: 0,
+      nature: -30,
+      culture: 0,
+      money: 0,
+      social: 20,
+    },
+    threshold: 10,
+    currentValue: 0
+  },
+  'www.buymystuffnow.com': {
+    task: '',
+    taskDescription: '',
+    resultDescription: '',
+    scores: {
+      security: 10,
+      nature: 0,
+      culture: 0,
+      money: 20,
+      social: -10,
+    },
+    threshold: 10,
+    currentValue: 0
+  },
+  'Let the sun shine!': {
+    task: '',
+    taskDescription: '',
+    resultDescription: '',
+    scores: {
+      security: 0,
+      nature: 30,
+      culture: 0,
+      money: -20,
+      social: 0,
+    },
+    threshold: 10,
+    currentValue: 0
+  }
+}
--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+const data = require('./data')
+const gameState = {
+  task1 = '',
+  task2 = '',
+  scores: {
+    security: 50,
+    nature: 50,
+    culture: 50,
+    money: 50,
+    social: 50,
+  },
+}
+
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
