eba616d3e780fdad2314de8fa5acdfdbd48d1c8d
IIYAMA12 <jorik@173.172.109.145.hva.nl>
2018-05-18 12:34:13 +0200

    add path for view engine

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 app.use(express.static('public'));
+app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res, next) => {
